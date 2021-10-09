7cff64e65283ec5cf7343528724e4452b7b69e65
baharudinmulya <baharu12185173@bsi.ac.id>
2021-10-09 13:23:24 +0700

    4

---
+++ Assets/script2.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script2 : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+    // Titik asal lintasan bola saat ini
+    private Vector2 trajectoryOrigin;
+    public float speed;
+
+    // Tombol untuk menggerakkan ke atas
+    public KeyCode upButton = KeyCode.W;
+
+    // Tombol untuk menggerakkan ke bawah
+    public KeyCode downButton = KeyCode.S;
+
+    // Tombol untuk menggerakkan ke kanan
+    public KeyCode rightButton = KeyCode.D;
+
+    // Tombol untuk menggerakkan ke kiri
+    public KeyCode leftButton = KeyCode.A;
+
+    public float yBoundary = 9.0f;
+
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        //ResetBall();
+
+        // Setelah 2 detik, berikan gaya ke bola
+        //Invoke("PushBall", 2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trajectoryOrigin = transform.position;
+        rigidBody2D = GetComponent<Rigidbody2D>();
+       Update();
+        // Mulai game
+        //RestartGame();
+    }
+
+    private void FixedUpdate()
+    {
+
+        // ballRigidbody.velocity.magnitude;
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+
+        if (rigidBody2D.velocity.magnitude != speed)
+        {
+            rigidBody2D.velocity = rigidBody2D.velocity.normalized * speed;
+        }
+    }
+
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+
+
+    // Untuk mengakses informasi titik asal lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        // Dapatkan kecepatan raket sekarang.
+        Vector2 velocity = rigidBody2D.velocity;
+
+        // Jika pemain menekan tombol ke atas, beri kecepatan positif ke komponen y (ke atas).
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+
+        // Jika pemain menekan tombol ke bawah, beri kecepatan negatif ke komponen y (ke bawah).
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+
+        // Jika pemain tidak menekan tombol apa-apa, kecepatannya nol.
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+
+        // Dapatkan posisi raket sekarang.
+        //Vector3 position = transform.position;
+
+        //BATAS ATAS KANAN
+
+        // Jika pemain menekan tombol ke atas, beri kecepatan positif ke komponen y (ke atas).
+        if (Input.GetKey(rightButton))
+        {
+            velocity.x = speed;
+        }
+
+        // Jika pemain menekan tombol ke bawah, beri kecepatan negatif ke komponen y (ke bawah).
+        else if (Input.GetKey(leftButton))
+        {
+            velocity.x = -speed;
+        }
+
+        // Jika pemain tidak menekan tombol apa-apa, kecepatannya nol.
+        else
+        {
+            velocity.x = 0.0f;
+        }
+
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+
+        // Dapatkan posisi raket sekarang.
+        Vector3 position = transform.position;
+
+     
+
+
+
+        // Masukkan kembali posisinya ke transform.
+        transform.position = position;
+
+
+
+    }
+}
