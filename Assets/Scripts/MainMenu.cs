43417ef06d9bb34721ad4804f2737e9baeffdebb
baharudinmulya <baharu12185173@bsi.ac.id>
2021-10-10 22:32:59 +0700

    Semua, pilih scene sesuai nomor saja jika ingin lihat urut

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Prob1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Prob2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void Prob3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void Prob4()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+    public void Prob5()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+    public void Prob6()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 6);
+    }
+    public void Prob7()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 7);
+    }
+    public void Prob8()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 8);
+    }
+    public void Prob9()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 9);
+    }
+
+    public void KeMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu1()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu2()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu3()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu4()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu5()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu6()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu7()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void KeMainMenu8()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadMainMenu()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/Prob5.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prob5 : MonoBehaviour
+{  
+    public float moveSpeed;
+    private bool hasHit = false;
+
+    public void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name.Equals("tiang2"))
+        {
+            moveSpeed = 0.5f;
+        }
+        if (collision.gameObject.name.Equals("tiang1"))
+        {
+            moveSpeed = 0.5f;
+        }
+        if (collision.gameObject.name.Equals("tiang3"))
+        {
+            moveSpeed = 0.5f;
+        }
+        if (collision.gameObject.name.Equals("tiang4"))
+        {
+            moveSpeed = 0.5f;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        moveSpeed = 3;
+    }
+
+}
