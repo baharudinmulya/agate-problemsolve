d266fe7fb3b060c7b21d16f123fe1834be6da2a8
baharudinmulya <baharu12185173@bsi.ac.id>
2021-10-10 21:24:50 +0700

    Semua, pilih scene sesuai nomor saja jika ingin lihat urut

---
+++ Assets/Scripts/Prob3.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prob3 : MonoBehaviour
+{
+    //inisialisasi rb2d
+    private Rigidbody2D rigidBody2D;
+
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        gaya();
+    }
+
+    void gaya()
+    {
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Prob6.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prob6 : MonoBehaviour
+{
+    public float moveSpeed;
+    private bool hasHit = false;
+
+    [SerializeField]
+    private GameObject star1;
+
+    [SerializeField]
+    private GameObject star2;
+
+    [SerializeField]
+    private GameObject star3;
+    public void Start()
+    {
+        randomSpawn();
+    }
+
+    public void randomSpawn()
+    {
+        var randNum = Random.Range(0, 3);
+        for (var i = 0; i < randNum; i++)
+        {
+            //Instantiate(star1, screenPosition, Quaternion.identity);
+            bool starSpawned = false;
+            while (!starSpawned)
+            {
+                Vector3 starPosition = new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0f);
+                if ((starPosition - transform.position).magnitude < 3)
+                {
+                    continue;
+                }
+                else
+                {
+
+                    Instantiate(star1, starPosition, Quaternion.identity);
+                    starSpawned = true;
+                }
+            }
+
+        }
+        for (var i = 0; i < randNum; i++)
+        {
+            bool starSpawned = false;
+            while (!starSpawned)
+            {
+                Vector3 starPosition = new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0f);
+                if ((starPosition - transform.position).magnitude < 3)
+                {
+                    continue;
+                }
+                else
+                {
+
+                    Instantiate(star2, starPosition, Quaternion.identity);
+                    starSpawned = true;
+                }
+            }
+        }
+    }
+
+    public void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name.Equals("tiang2"))
+        {
+            moveSpeed = 0.5f;
+        }
+        if (collision.gameObject.name.Equals("tiang1"))
+        {
+            moveSpeed = 0.5f;
+        }
+        if (collision.gameObject.name.Equals("tiang3"))
+        {
+            moveSpeed = 0.5f;
+        }
+        if (collision.gameObject.name.Equals("tiang4"))
+        {
+            moveSpeed = 0.5f;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        moveSpeed = 3;
+    }
+
+}
